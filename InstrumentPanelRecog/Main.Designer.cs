276c6a158f9b9447c1ae0c404fb8a0f56d7e3f30
AFSY95122 <sy95122@163.com>
2020-04-09 17:39:55 +0800

    002

--- InstrumentPanelRecog/Main.Designer.cs
+++ InstrumentPanelRecog/Main.Designer.cs
@@ -41,6 +41,7 @@
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.txtNotifyInfo = new System.Windows.Forms.TextBox();
             this.timerVideo = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.menuMain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -51,27 +52,30 @@
             // 
             // menuMain
             // 
+            this.menuMain.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.menuSetupComm});
             this.menuMain.Location = new System.Drawing.Point(0, 0);
             this.menuMain.Name = "menuMain";
-            this.menuMain.Size = new System.Drawing.Size(1058, 25);
+            this.menuMain.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuMain.Size = new System.Drawing.Size(1411, 28);
             this.menuMain.TabIndex = 0;
             this.menuMain.Text = "menuMain";
             // 
             // menuSetupComm
             // 
             this.menuSetupComm.Name = "menuSetupComm";
-            this.menuSetupComm.Size = new System.Drawing.Size(68, 21);
+            this.menuSetupComm.Size = new System.Drawing.Size(83, 24);
             this.menuSetupComm.Text = "通信配置";
             this.menuSetupComm.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Gray;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 28);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 35);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.Size = new System.Drawing.Size(1067, 750);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -80,9 +84,11 @@
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.tabPanel);
-            this.groupBox1.Location = new System.Drawing.Point(818, 133);
+            this.groupBox1.Location = new System.Drawing.Point(1091, 166);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(228, 325);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(304, 406);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "表盘模型";
@@ -94,28 +100,31 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tabPanel.Controls.Add(this.tabPageSingle);
             this.tabPanel.Controls.Add(this.tabPageMultiple);
-            this.tabPanel.Location = new System.Drawing.Point(6, 20);
+            this.tabPanel.Location = new System.Drawing.Point(8, 25);
+            this.tabPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPanel.Name = "tabPanel";
             this.tabPanel.SelectedIndex = 0;
-            this.tabPanel.Size = new System.Drawing.Size(216, 299);
+            this.tabPanel.Size = new System.Drawing.Size(288, 374);
             this.tabPanel.TabIndex = 0;
             // 
             // tabPageSingle
             // 
-            this.tabPageSingle.Location = new System.Drawing.Point(4, 22);
+            this.tabPageSingle.Location = new System.Drawing.Point(4, 25);
+            this.tabPageSingle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPageSingle.Name = "tabPageSingle";
-            this.tabPageSingle.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageSingle.Size = new System.Drawing.Size(208, 273);
+            this.tabPageSingle.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPageSingle.Size = new System.Drawing.Size(280, 345);
             this.tabPageSingle.TabIndex = 0;
             this.tabPageSingle.Text = "单表盘";
             this.tabPageSingle.UseVisualStyleBackColor = true;
             // 
             // tabPageMultiple
             // 
-            this.tabPageMultiple.Location = new System.Drawing.Point(4, 22);
+            this.tabPageMultiple.Location = new System.Drawing.Point(4, 25);
+            this.tabPageMultiple.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPageMultiple.Name = "tabPageMultiple";
-            this.tabPageMultiple.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageMultiple.Size = new System.Drawing.Size(208, 273);
+            this.tabPageMultiple.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPageMultiple.Size = new System.Drawing.Size(280, 345);
             this.tabPageMultiple.TabIndex = 1;
             this.tabPageMultiple.Text = "多表盘";
             this.tabPageMultiple.UseVisualStyleBackColor = true;
@@ -124,19 +133,23 @@
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.btnCam);
-            this.groupBox2.Location = new System.Drawing.Point(818, 28);
+            this.groupBox2.Location = new System.Drawing.Point(1091, 35);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(228, 99);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(304, 124);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "相机控制";
             // 
             // btnCam
             // 
-            this.btnCam.Location = new System.Drawing.Point(25, 20);
+            this.btnCam.Location = new System.Drawing.Point(33, 25);
+            this.btnCam.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCam.Name = "btnCam";
-            this.btnCam.Size = new System.Drawing.Size(75, 23);
+            this.btnCam.Size = new System.Drawing.Size(100, 29);
             this.btnCam.TabIndex = 0;
             this.btnCam.Text = "打开相机";
             this.btnCam.UseVisualStyleBackColor = true;
@@ -145,9 +158,11 @@
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.txtNotifyInfo);
-            this.groupBox3.Location = new System.Drawing.Point(818, 464);
+            this.groupBox3.Location = new System.Drawing.Point(1091, 580);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(228, 164);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(304, 205);
             this.groupBox3.TabIndex = 4;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "通知消息";
@@ -157,25 +172,35 @@
             this.txtNotifyInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtNotifyInfo.Location = new System.Drawing.Point(6, 20);
+            this.txtNotifyInfo.Location = new System.Drawing.Point(8, 25);
+            this.txtNotifyInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNotifyInfo.Multiline = true;
             this.txtNotifyInfo.Name = "txtNotifyInfo";
             this.txtNotifyInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtNotifyInfo.Size = new System.Drawing.Size(216, 138);
+            this.txtNotifyInfo.Size = new System.Drawing.Size(287, 172);
             this.txtNotifyInfo.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1058, 639);
+            this.ClientSize = new System.Drawing.Size(1411, 799);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuMain);
             this.MainMenuStrip = this.menuMain;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -207,6 +232,7 @@
         private System.Windows.Forms.TabPage tabPageSingle;
         private System.Windows.Forms.TabPage tabPageMultiple;
         private System.Windows.Forms.Timer timerVideo;
+        private System.Windows.Forms.Button button1;
     }
 }
 
