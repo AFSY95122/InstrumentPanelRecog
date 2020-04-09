f79967a9671236d20a2077261987aa6e8285fd7b
AFSY95122 <sy95122@163.com>
2020-04-09 16:52:53 +0800

    v.001

---
+++ InstrumentPanelRecog/Main.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace InstrumentPanelRecog
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuMain = new System.Windows.Forms.MenuStrip();
+            this.menuSetupComm = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tabPanel = new System.Windows.Forms.TabControl();
+            this.tabPageSingle = new System.Windows.Forms.TabPage();
+            this.tabPageMultiple = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCam = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtNotifyInfo = new System.Windows.Forms.TextBox();
+            this.timerVideo = new System.Windows.Forms.Timer(this.components);
+            this.menuMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.tabPanel.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuMain
+            // 
+            this.menuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuSetupComm});
+            this.menuMain.Location = new System.Drawing.Point(0, 0);
+            this.menuMain.Name = "menuMain";
+            this.menuMain.Size = new System.Drawing.Size(1058, 25);
+            this.menuMain.TabIndex = 0;
+            this.menuMain.Text = "menuMain";
+            // 
+            // menuSetupComm
+            // 
+            this.menuSetupComm.Name = "menuSetupComm";
+            this.menuSetupComm.Size = new System.Drawing.Size(68, 21);
+            this.menuSetupComm.Text = "通信配置";
+            this.menuSetupComm.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.tabPanel);
+            this.groupBox1.Location = new System.Drawing.Point(818, 133);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 325);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "表盘模型";
+            // 
+            // tabPanel
+            // 
+            this.tabPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabPanel.Controls.Add(this.tabPageSingle);
+            this.tabPanel.Controls.Add(this.tabPageMultiple);
+            this.tabPanel.Location = new System.Drawing.Point(6, 20);
+            this.tabPanel.Name = "tabPanel";
+            this.tabPanel.SelectedIndex = 0;
+            this.tabPanel.Size = new System.Drawing.Size(216, 299);
+            this.tabPanel.TabIndex = 0;
+            // 
+            // tabPageSingle
+            // 
+            this.tabPageSingle.Location = new System.Drawing.Point(4, 22);
+            this.tabPageSingle.Name = "tabPageSingle";
+            this.tabPageSingle.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageSingle.Size = new System.Drawing.Size(208, 273);
+            this.tabPageSingle.TabIndex = 0;
+            this.tabPageSingle.Text = "单表盘";
+            this.tabPageSingle.UseVisualStyleBackColor = true;
+            // 
+            // tabPageMultiple
+            // 
+            this.tabPageMultiple.Location = new System.Drawing.Point(4, 22);
+            this.tabPageMultiple.Name = "tabPageMultiple";
+            this.tabPageMultiple.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageMultiple.Size = new System.Drawing.Size(208, 273);
+            this.tabPageMultiple.TabIndex = 1;
+            this.tabPageMultiple.Text = "多表盘";
+            this.tabPageMultiple.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.btnCam);
+            this.groupBox2.Location = new System.Drawing.Point(818, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(228, 99);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "相机控制";
+            // 
+            // btnCam
+            // 
+            this.btnCam.Location = new System.Drawing.Point(25, 20);
+            this.btnCam.Name = "btnCam";
+            this.btnCam.Size = new System.Drawing.Size(75, 23);
+            this.btnCam.TabIndex = 0;
+            this.btnCam.Text = "打开相机";
+            this.btnCam.UseVisualStyleBackColor = true;
+            this.btnCam.Click += new System.EventHandler(this.btnCam_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtNotifyInfo);
+            this.groupBox3.Location = new System.Drawing.Point(818, 464);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(228, 164);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "通知消息";
+            // 
+            // txtNotifyInfo
+            // 
+            this.txtNotifyInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNotifyInfo.Location = new System.Drawing.Point(6, 20);
+            this.txtNotifyInfo.Multiline = true;
+            this.txtNotifyInfo.Name = "txtNotifyInfo";
+            this.txtNotifyInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtNotifyInfo.Size = new System.Drawing.Size(216, 138);
+            this.txtNotifyInfo.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 639);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuMain);
+            this.MainMenuStrip = this.menuMain;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "指针表盘识别";
+            this.menuMain.ResumeLayout(false);
+            this.menuMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.tabPanel.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuMain;
+        private System.Windows.Forms.ToolStripMenuItem menuSetupComm;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCam;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtNotifyInfo;
+        private System.Windows.Forms.TabControl tabPanel;
+        private System.Windows.Forms.TabPage tabPageSingle;
+        private System.Windows.Forms.TabPage tabPageMultiple;
+        private System.Windows.Forms.Timer timerVideo;
+    }
+}
+
---
+++ InstrumentPanelRecog/Main.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using OpenCvSharp.UserInterface;
+
+namespace InstrumentPanelRecog
+{
+    public partial class Main : Form
+    {
+        bool m_bCamIsOpened = false;
+        static VideoCapture m_cam = new VideoCapture(); // 配置管理器不能是anyCPU
+        Mat m_panelTemplateImg = new Mat();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public void Info(string strInfo)
+        {
+            string strDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            txtNotifyInfo.AppendText("\r\n" + strDateTime + " " + strInfo);                 // add new message
+            txtNotifyInfo.SelectionStart = txtNotifyInfo.TextLength;    // move cursor to last
+            txtNotifyInfo.ScrollToCaret();                              // always display new line
+        }
+
+        private void btnCam_Click(object sender, EventArgs e)
+        {
+            if (!m_bCamIsOpened) // 打开相机
+            {
+                m_cam.Open(0);
+                if (m_cam.IsOpened())
+                {
+                    m_bCamIsOpened = true;
+                    btnCam.Text = "关闭相机";
+                    Info("打开相机。");
+                }
+                else
+                {
+                    Info("无法打开相机，请检查相机是否连接！");
+                    return;
+                }
+            }
+            else // 关闭相机
+            {
+                m_cam.Dispose();
+
+                if (m_cam.IsDisposed)
+                {
+                    m_bCamIsOpened = false;
+                    btnCam.Text = "打开相机";
+                    Info("关闭相机。");
+                }
+            }
+        }
+    }
+}
